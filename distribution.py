0a6097980445fae711570bb828cdb0ee587cd64f
Cristian Echeverría <cer1969@gmail.com>
2009-04-16 17:15:52 -0400

    Carga inicial

---
+++ distribution.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# CRISTIAN ECHEVERRÍA RABÍ 
+
+from distutils.core import setup
+import py2exe
+import sys
+
+#-----------------------------------------------------------------------------------------
+
+__all__ = ["Distribution", "get_options", "make_exe"]
+
+#-----------------------------------------------------------------------------------------
+
+manifest_template = '''
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<assemblyIdentity
+    version="5.0.0.0"
+    processorArchitecture="x86"
+    name="%(name)s"
+    type="win32"
+/>
+<description>%(name)s Program</description>
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="X86"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+</dependency>
+</assembly>
+'''
+
+RT_MANIFEST = 24
+
+#-----------------------------------------------------------------------------------------
+
+class Distribution(object):
+    
+    def __init__(self, info, manifest=True):
+        
+        self.name = info.name
+        self.version = info.version
+        self.description = info.description
+        self.company_name = info.company
+        self.copyright = info.copyright
+        
+        # start script 
+        self.script = "%(name)s.py" % info
+        
+        if manifest:
+            self.other_resources = [(RT_MANIFEST, 1, manifest_template % info)]
+        if info.iconpath:
+            self.icon_resources = [(1, info.iconpath)]
+        
+        # exe name
+        self.dest_base = info.name
+
+#-----------------------------------------------------------------------------------------
+
+def get_options(**kwa):
+    sal = {}
+    sal['py2exe'] = kwa
+    return sal
+
+#-----------------------------------------------------------------------------------------
+
+def make_exe(**kwa):
+    # Cuando se corre sin argumentos, incorporamos py2exe as argument
+    sys.argv.append("py2exe")
+    setup(**kwa)
---
+++ translator.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+# CRISTIAN ECHEVERRÍA RABÍ
+
+import os, glob
+
+#-----------------------------------------------------------------------------------------
+
+NO_LANG = "no_lang"
+
+#-----------------------------------------------------------------------------------------
+
+class Translator(dict):
+    
+    def __init__(self):
+        dict.__init__(self)
+        self._folder = None
+        self._lang = NO_LANG
+        self.name = None
+        self._nocat = {} # To store missing keys
+    
+    #-------------------------------------------------------------------------------------
+    # Public methods
+    
+    def translate(self, key):
+        if self._lang == NO_LANG:
+            return key
+        try:
+            return self[key]
+        except KeyError:
+            self._nocat[key] = key
+            return key
+    
+    def save(self):
+        if self._lang == NO_LANG:
+            raise AttributeError("'no_lang' is not a valid catalog name")
+        if self._folder is None:
+            raise AttributeError("None is not a valid folder name")
+        
+        if not(os.path.exists(self._folder)):
+            os.mkdir(self._folder)
+        
+        langfile = os.path.join(self.folder, "%s.cat" % self._lang)
+        f = open(langfile, "w")
+        f.write(self._getText())
+        f.close()
+    
+    def getLangList(self):
+        if self._folder is None:
+            raise AttributeError("None is not a valid folder name")
+        lcats = glob.glob(os.path.join(self._folder, "*.cat"))
+        lcats = [os.path.split(x)[1] for x in lcats]
+        lcats = [os.path.splitext(x)[0] for x in lcats]
+        sal = []
+        for i in lcats:
+            name, cat = self._getCatalog(i)
+            sal.append((i, name))
+        return sal
+    
+    #-------------------------------------------------------------------------------------
+    # Private methods
+
+    def _getCatalog(self, lang):
+        langfile = os.path.join(self.folder, "%s.cat" % lang)
+        mydict = {"name": None, "cat": {}}
+        try:
+            execfile(langfile, {}, mydict)
+        except IOError:
+            pass
+        return mydict["name"], mydict["cat"]
+    
+    def _getText(self):
+        tpl = ("# -*- coding: utf-8 -*-\n"
+               "# Language catalog\n\n"
+               "name = %s\n\n"
+               "cat  = {\n%s\n}\n\n"
+               "new  = {\n%s\n}"
+        )
+        
+        tname = repr(self.name)
+        
+        catitems = self.items()
+        catitems.sort()
+        catkeys = ["    %s: %s" % (repr(key), repr(value)) for (key,value) in catitems]
+        tcat  = ",\n".join(catkeys)
+        
+        newitems = self._nocat.items()
+        newitems.sort()
+        newkeys = ["    %s: %s" % (repr(key), repr(value)) for (key,value) in newitems]
+        tnew = ",\n".join(newkeys)
+        
+        sal = tpl % (tname, tcat, tnew)
+        return sal
+    
+    #-------------------------------------------------------------------------------------
+    # Properties methods
+    
+    def _getFolder(self):
+        return self._folder
+    
+    def _setFolder(self, folder):
+        self._folder = os.path.abspath(folder)
+    
+    def _getLang(self):
+        return self._lang
+    
+    def _setLang(self, lang):
+        self.clear()
+        self._lang = lang
+        if lang == NO_LANG:
+            self.name = None
+        else:
+            name, cat = self._getCatalog(lang)
+            self.name = name
+            self.update(cat)
+    
+    #-------------------------------------------------------------------------------------
+    # Properties
+    
+    folder = property(_getFolder, _setFolder)
+    lang   = property(_getLang,   _setLang)
