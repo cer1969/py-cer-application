e85b637fecc0431df0758aefad2e051f7b8beec7
Cristian Echeverría <cer1969@gmail.com>
2010-04-01 16:02:33 -0300

    Better implementation of Application

--- application.py
+++ application.py
@@ -39,54 +39,62 @@ MANIFEST_RT = 24
 
 #-----------------------------------------------------------------------------------------
 
+isexe = hasattr(sys,"frozen")
+argv = sys.argv[1:]
+
+_app_path = sys.executable if isexe else sys.argv[0]
+_app_path = os.path.abspath(_app_path)
+appdir = os.path.dirname(_app_path)
+
+#-----------------------------------------------------------------------------------------
+
 class Application(object):
     
-    def __init__(self, name, version, copyright="", company_name=None, description=None,
-            iconpath=None, script=None, dest_base=None, manifest=False
-        ):
+    _isexe = isexe
+    _argv = argv
+    _appdir = appdir
+    
+    def __init__(self, name, version, copyright="", company_name=None, description=None):
         """
         name         : Application name
         version      : Application version
         copyright    : Optional copyright notice
         company_name : Optional company name
         description  : Optional Application description
-        iconpath     : Optional icon path (py2exe)
-        script       : Optional main python script name (py2exe)
-        dest_base    : Optional executable name (py2exe)
-        manifest     : Optional True to embed manifest. Use false for python 2.6 (py2exe)
+        
+        Read/write members
+        iconpath     : Icon path (py2exe)
+        script       : Main python script name. Default name.py (py2exe)
+        dest_base    : Executable name. Default name (py2exe)
+        manifest     : True to embed manifest. Use false for python 2.6 (py2exe)
+        ini          : Optional AppIni object. Default None
+        trans        : Optional Translator object. Default None
+        
+        Readonly members (class members: All instance has the same values)
+        isexe        : True if compiled with py2exe
+        argv         : Command line arguments list
+        appdir       : directory of caller script or exe
         """
         self.name = name
         self.version = version
         self.copyright = copyright
-        
         self.company_name = company_name if company_name else copyright
         self.description = description if description else (u"%s Application" % name)
-        self.iconpath = iconpath
-        self.script = script if script else ("%s.py" % name)
-        self.dest_base = dest_base if dest_base else name
-        self.manifest = manifest 
         
-        if iconpath:
-            self.icon_resources = [(1, iconpath)]
-        if manifest:
-            self.other_resources = [(MANIFEST_RT, 1, MANIFEST_TPL % (name, self.description))]
-        
+        self._iconpath = None
+        self.script = "%s.py" % name
+        self.dest_base = name
+        self._manifest = False 
         self.ini = None
-        self._trans = None
-        
-        self.isexe = hasattr(sys,"frozen") # True if compiled with py2exe
-        self.argv = sys.argv[1:] # Command line arguments list
-        
-        app_path = sys.executable if self.isexe else sys.argv[0]
-        app_path = os.path.abspath(app_path)
-        self.appdir = os.path.dirname(app_path) # directory of caller script or exe        
+        self._trans = None        
     
     #-------------------------------------------------------------------------------------
     # Public methods
     
-    def toAppDir(self, pathname):
+    @staticmethod
+    def toAppDir(pathname):
         """Return pathname joined with appdir"""
-        p = os.path.join(self.appdir, pathname)
+        p = os.path.join(Application._appdir, pathname)
         return os.path.abspath(p)
     
     def register(self, name):
@@ -94,16 +102,55 @@ class Application(object):
         __builtin__.__dict__[name] = self
     
     #-------------------------------------------------------------------------------------
-    # Properties
+    # Readonly properties
     
+    @property
+    def isexe(self):
+        return self._isexe
+
+    @property
+    def argv(self):
+        return self._argv    
+    
+    @property
+    def appdir(self):
+        return self._appdir
+    
+    #-------------------------------------------------------------------------------------
+    # Read/write properties
+    
+    def _getIconPath(self):
+        return self._iconpath
+    
+    def _setIconPath(self, iconpath):
+        self._iconpath = iconpath
+        if (iconpath is None) and hasattr(self, "icon_resources"):
+            delattr(self, "icon_resources")
+        else:
+            self.icon_resources = [(1, iconpath)]
+    
+    iconpath = property(_getIconPath, _setIconPath)
+    
+    def _getManifest(self):
+        return self._manifest
+    
+    def _setManifest(self, manifest):
+        self._manifest = manifest
+        if (manifest is None) and hasattr(self, "other_resources"):
+            delattr(self, "other_resources")
+        else:
+            tpl = MANIFEST_TPL % (self.name, self.description)
+            self.other_resources = [(MANIFEST_RT, 1, tpl)]
+    
+    manifest = property(_getManifest, _setManifest)
+        
     def _getTrans(self):
         return self._trans
     
     def _setTrans(self, trans):
         self._trans = trans
-        if trans is None:
-            if "_" in __builtin__.__dict__:
-                del __builtin__.__dict__["_"]
+        if (trans is None) and ("_" in __builtin__.__dict__):
+            del __builtin__.__dict__["_"]
         else:
             __builtin__.__dict__["_"] = self._trans.translate
     
